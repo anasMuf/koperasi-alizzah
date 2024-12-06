6db7f25229c6cadf1626f9a3888a1a1bc3757abb
anasMuf <anas.muhammadakbar@gmail.com>
2024-12-06 10:48:20 +0700

    update version

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 1.0.0
+        <b>Version</b> 1.1.3
     </div>
     <strong>Koperasi <a href="https://alizzah.org">Al-Izzah</a>.</strong>
 </footer>
